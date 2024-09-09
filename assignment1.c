2d5220a0c8b15f001ea26dfb13e7ce1a317023ad
aryashinde9099 <shindearya9099@gmail.com>
2024-09-09 14:22:38 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,13 @@
+#include<stdio.h.>
+void main(){
+    int num=10;
+    char ch='s';
+    float rs=55.22;
+    double crMoney=542154313480.544543;
+
+    printf("%d\n",num);
+    printf("%c\n",ch);
+    printf("%f\n",rs);
+    printf("%lf\n",crMoney);
+  
+}
